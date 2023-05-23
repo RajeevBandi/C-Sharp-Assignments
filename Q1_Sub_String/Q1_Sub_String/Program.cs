634958c351b75778eb565cb1314ae7f754aa2cfa
Rajeev <Rajeev.Bandi@cognine.com>
2023-05-23 16:35:14 +0530

    Q1 commit

---
+++ Q1_Sub_String/Q1_Sub_String/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1_Sub_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SubstringClass obj1 = new SubstringClass("abcabcabc", "ab");
+            obj1.helper();
+        }
+    }
+}
---
+++ Q1_Sub_String/Q1_Sub_String/SubstringClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1_Sub_String
+{
+    public class SubstringClass
+    {
+        int noOfTimesOccured = 0;
+        string s1, s2, temp;
+        public SubstringClass(string s1, string s2)
+        {
+            this.s1 = s1;
+            this.s2 = s2;
+            for (int i = 0; i < s1.Length - s2.Length; i++)
+            {
+                temp = (s1.Substring(i, s2.Length));
+                if (temp == s2)
+                {
+                    noOfTimesOccured += 1;
+                }
+            }
+        }
+
+        public void helper()
+        {
+            Console.WriteLine("No Of Times occured = " + noOfTimesOccured);
+            Console.Write("Index Position : ");
+            for (int i = 0; i < s1.Length - s2.Length; i++)
+            {
+                temp = (s1.Substring(i, s2.Length));
+                if (temp == s2)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
